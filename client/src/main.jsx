05bef4e6824bc4d58cfa1d16ea4eeba68d3c42dd
“Thomas <tjwhitephd@gmail.com>
2025-01-21 19:35:48 -0800

    bout to push some JS up in this ...

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,1 +1,35 @@
 // Creating the router
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import App from "./App.jsx";
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./pages/SavedBooks";
+import SwapBooks from "./pages/SwapBooks";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <h1 className="display-2">Wrong page!</h1>,
+    children: [
+      {
+        index: true,
+        element: <SearchBooks />,
+      },
+      {
+        path: "/saved",
+        element: <SavedBooks />,
+      },
+      {
+        path: "/swap",
+        element: <SwapBooks />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
