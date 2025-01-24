816ebd842abe9375975d57a9bdd808f6fd810f24
Kun Wansom <wansom.kun@gmail.com>
2025-01-23 20:23:52 -0800

    added package-lock

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,75 +1,91 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const LOGINUSER = gql`
-    mutation loginUser($email: String!, $password: String!) {
-        login(email: $email, password: $password) {
-            token
-            user {
-                _id
-                username
-            }
-        }
+  mutation loginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
     }
+  }
 `;
 
 export const ADD_USER = gql`
-    mutation addUser($username: String!, $email: String!, $password: String!, $street: String!, $city: String!, $state:, String!, $zipCode: String!) {
-        addUser(username: $username, email: $email, password: $password, street: $street, city: $city, state: $state, zipCode: $zipCode ) {
-            token
-            user {
-                _id
-                username
-                email
-                street
-                city
-                state
-                zipCode
-                bookCount
-                savedBooks {
-                    authors
-                    bookId
-                    image
-                    link
-                    title
-                    description
-                }
-            }
-        } 
+  mutation addUser(
+    $username: String!
+    $email: String!
+    $password: String!
+    $street: String!
+    $city: String!
+    $state: String!
+    $zipCode: String!
+  ) {
+    addUser(
+      username: $username
+      email: $email
+      password: $password
+      street: $street
+      city: $city
+      state: $state
+      zipCode: $zipCode
+    ) {
+      token
+      user {
+        _id
+        username
+        email
+        street
+        city
+        state
+        zipCode
+        bookCount
+        savedBooks {
+          authors
+          bookId
+          image
+          link
+          title
+          description
+        }
+      }
     }
+  }
 `;
 
 export const SAVEBOOK = gql`
-    mutation saveBook($newBook: InputBook!) {
-        saveBook(newBook: $newBook) {
-            _id
-            username
-            email
-            savedBooks {
-                bookId
-                authors
-                description
-                title
-                image
-                link
-            }
-        }
+  mutation saveBook($newBook: InputBook!) {
+    saveBook(newBook: $newBook) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
     }
+  }
 `;
 
 export const REMOVEBOOK = gql`
-    mutation removeBook($bookId: ID!) {
-        removeBook(bookId: $bookId) {
-            _id
-            username
-            email
-            savedBooks {
-                bookId
-                authors
-                description
-                title
-                image
-                link
-            }
-        }
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
     }
+  }
 `;
